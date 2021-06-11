ae05e7714f3891ce9025ae97c52d5cb3c3e33e5e
smilewendy7 <smilewendy7@gmail.com>
2021-06-11 16:51:02 -0400

    easy mode basically done

--- mysteryword.py
+++ mysteryword.py
@@ -5,7 +5,7 @@ def mystery_word(file):
         print(text)
 
         ## level 1 words
-        l1_words = [ word for word in text if len(word)>=4 and len(word)<=6]
+        l1_words = [ word for word in text if len(word)>=6]
 
         input_level= input("Choose a level of difficulty")
 
@@ -17,33 +17,32 @@ def mystery_word(file):
 
             guess_input= input("Please guess 1 letter per round")
 
-            list_selected_word = [char for char in selected_word]
+            # list_selected_word = [char for char in selected_word]
             # print(list_selected_word)
-            output_word= []
+            output_word= ""
             i=0
-            while output_word != list_selected_word:
+            guess_collection = []
+            while output_word != selected_word:
+                output_word = selected_word
                 i+=1
+                if guess_input in selected_word:
+                    guess_collection.append(guess_input)
+                print(f" here is {guess_collection}")
                 for letter in selected_word:
-                    if letter == guess_input and i==1:
-                        output_word.append(letter)
-                    elif i==1:
-                        output_word.append("_")
-                    elif letter == guess_input:
-                        j=(selected_word.index(letter))
-                        output_word[j] = letter
-                    else:
-                        output_word
-                print(output_word)
-                if i==7:
-                    print("Sorry, you lose!!")
-                else:
-                    guess_input= input("Please guess 1 letter per round")
-                
+                    if letter not in guess_collection:
+                        output_word = output_word.replace(letter, "_")
+                        
+                   
 
-            if output_word == list_selected_word:
-                print("you win!")
-            
-            
+                print(output_word)
+                if output_word == selected_word:
+                    print("you win!")
+                    break
+                if i==8:
+                    print("Sorry, you lose!!")
+                    break
+                else:
+                    guess_input= input(f"Please guess 1 letter per round, {8-i} times left to try!")
         
 
         
